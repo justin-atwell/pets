67f1ea7ccb68bd9b4c0d4cf3890bff33390ab3fe
McKim <AAM1022@daugherty.com>
2020-01-31 11:41:27 -0600

    stubbed out the directories for services, controllers, and repos

---
+++ src/main/java/com/pets/configuration/AppConfig.java
@@ -0,0 +1,12 @@
+package com.pets.configuration;
+
+//import Repo interface
+//import Repo Impl
+//import Service interface
+//import Service Impl
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class AppConfig {
+}
