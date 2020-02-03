1f6947c46dbd43607b77e75c969a111f0cb2ed41
PaulBollinger <paul.bollinger@daugherty.com>
2020-02-03 09:56:29 -0600

    setup selenium

---
+++ src/main/java/com/pets/pages/HomePage.java
@@ -0,0 +1,4 @@
+package com.pets.pages;
+
+public class HomePage {
+}
---
+++ src/test/java/selenium/tests/BaseTests.java
@@ -0,0 +1,13 @@
+package selenium.tests;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTests {
+    ChromeDriver chromeDriver = new ChromeDriver();
+
+    @Test
+    public void openBrowser() {
+        chromeDriver.get("http://www.google.com");
+    }
+}
