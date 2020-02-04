54d4176987f16a1bd6b2085df1e501b2b974b579
McKim <AAM1022@daugherty.com>
2020-02-04 08:35:49 -0600

    stripped extra quotes from returned localDateTime in controller tests

--- src/test/java/com/pets/controllers/HealthCheckControllerTests.java
+++ src/test/java/com/pets/controllers/HealthCheckControllerTests.java
@@ -1,9 +1,7 @@
 package com.pets.controllers;
 
-import com.mysql.cj.util.TestUtils;
+
 import com.pets.services.HealthCheckService;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.core.IsEqual;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,27 +10,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Date;
 
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.hamcrest.Matchers.instanceOf;
-import static org.hamcrest.core.Is.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -50,51 +34,27 @@ public class HealthCheckControllerTests {
     }
     @Test
     public void getHealthCheckReturnsCorrectDate() throws Exception {
-        //String dateFormatter = "yyyy-MM-dd HH:mm:ss";
-        //String dateFormatter = "yyyy-MM-dd HH:mm:ss.SSS";
-        //DateTimeFormatter formatter = DateTimeFormatter.ofPattern(dateFormatter);
-
         LocalDateTime rightNow = LocalDateTime.now();
 
         when(service.getHealthCheckDate()).thenReturn(rightNow);
 
-        MvcResult result = mockMvc.perform(get("/healthCheck"))
+        MockHttpServletResponse result = mockMvc.perform(get("/healthCheck"))
             .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-            .andReturn();
-            .getResponse()
-            //.getContentAsString();
+            .andReturn()
+            .getResponse();
 
-        String content = result.getResponse().getContentAsString();
+        String content = result.getContentAsString();
+        Integer endIndex = content.length() -1;
+        String substring = content.substring(1, endIndex);
 
         System.out.println("Right now as LocalDateTime: " + rightNow);
         System.out.println("Right now as string: " + rightNow.toString());
         System.out.println(("Result: " + result));
-        System.out.println("Content as String: " + content);
+        System.out.println("Content: " + content);
+        System.out.println("Substring: " + substring);
 
-        //LocalDateTime formattedResult = LocalDateTime.parse(result, formatter);
-        LocalDateTime formattedContent = LocalDateTime.parse(content, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+        LocalDateTime formattedContent = LocalDateTime.parse(substring, DateTimeFormatter.ISO_LOCAL_DATE_TIME);
+
         System.out.println("Result as LocalDateTime: " + formattedContent);
     }
-    /*@Test
-    public void getHealthCheckReturnsCurrentDate() throws Exception {
-        LocalDateTime rightNow = LocalDateTime.now();
-        when(service.getHealthCheckDate()).thenReturn(rightNow);
-        String result = mockMvc.perform((get("/healthCheck")))
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andReturn()
-                .getResponse()
-                .getContentAsString();
-
-        System.out.println("Right now date:" + rightNow);
-        System.out.println("Right now string:" + rightNow.toString());;
-        System.out.println("Result:" + result);
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS");
-
-        LocalDateTime formattedTime = LocalDateTime.parse(result, formatter);
-
-        System.out.println("Result as LocalDateTime:" + formattedTime);
-
-        assertEquals(result, rightNow);
-    }*/
 }
